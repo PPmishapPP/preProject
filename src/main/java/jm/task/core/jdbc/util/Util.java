297e58d9607e4804505bd84555b02098f74acce9
PPmishapPP <ka4erp@mail.ru>
2020-12-21 12:07:51 +0500

    Вроде всё

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/test";
+        String userName = "root";
+        String password = "setchatka";
+
+        return DriverManager.getConnection(url, userName, password);
+    }
 }
